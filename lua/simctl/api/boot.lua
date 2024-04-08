ec268bd8410ffbb52199cbb521917735379fc765
Terje Tjervaag <terje.tjervaag@youwell.no>
2024-04-08 21:28:46 +0200

    fix: Don't autoselect booted device for boot function

--- lua/simctl/api/boot.lua
+++ lua/simctl/api/boot.lua
@@ -10,27 +10,29 @@ local config = require("simctl.config")
 -- @param args.deviceId string ID of the simulator to affect
 -- @param callback function indicating success or failure
 M.boot = function(args, callback)
-	callback = callback or function() end
-	args = args or {}
+  callback = callback or function() end
+  args = args or {}
 
-	aw.async(function()
-		if args.deviceId == nil and config.options.devicePicker then
-			args.deviceId = aw.await(pickers.pickDevice)
-		end
+  aw.async(function()
+    if args.deviceId == nil and config.options.devicePicker then
+      args.deviceId = aw.await(pickers.pickDevice)
+    end
 
-		args = util.merge(args, {
-			deviceId = "booted",
-		})
+    if args.deviceId == nil then
+      util.notify("No device selected", vim.log.levels.ERROR)
+      callback(false)
+      return
+    end
 
-		simctl.execute({ "boot", args.deviceId }, function(return_val, humane, stdout, stderr)
-			if return_val ~= 0 then
-				local message = humane or stderr
-				util.notify(message)
-			end
+    simctl.execute({ "boot", args.deviceId }, function(return_val, humane, stdout, stderr)
+      if return_val ~= 0 then
+        local message = humane or stderr
+        util.notify(message)
+      end
 
-			callback(return_val == 0, nil, stdout, stderr)
-		end)
-	end)
+      callback(return_val == 0, nil, stdout, stderr)
+    end)
+  end)
 end
 
 return M
